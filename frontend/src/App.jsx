861d7db631e8a926c185dc24173faa8243a5d9e3
Delia Movila <delia.movila14@gmail.com>
2026-05-12 12:51:14 +0300

    Changed search button color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
 
         <button
           onClick={getWeather}
-          className="bg-blue-600 text-white px-6 py-4 rounded-xl text-lg font-bold hover:bg-blue-700 transition"
+          className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded"
         >
           Search
         </button>
